dcefd8082750b8a1c2113527c95a9a63630c7382
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2017-01-31 22:19:45 +0200

    add component event communication

--- events.js
+++ events.js
@@ -20,3 +20,29 @@ var app = new Vue({
         }
     }
 });
+
+Vue.component('button-counter', {
+  template: '<button @click="incrementme">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementme: function () {
+      this.counter += 1;
+      this.$emit('increment');
+    }
+  },
+})
+new Vue({
+  el: '#counter-event-example',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1;
+    }
+  }
+});
